0ca089ec2e72260a7a43da1280718150998546da
Vukasin Jankovic <v.jankovic@vegait.rs>
2021-09-24 22:29:25 +0200

    RecommendationBox formatted - Added global formatting settings

--- src/components/recommendationBox/RecommendationBox.tsx
+++ src/components/recommendationBox/RecommendationBox.tsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import { View, Image } from 'react-native'
-import { SvgProps } from 'react-native-svg';
-import { CustomText } from '../customText/CustomText';
+import React from 'react';
+import {View} from 'react-native';
+import {SvgProps} from 'react-native-svg';
+import {CustomText} from '../customText/CustomText';
 import style from './style';
 
-const timerIconPath = require('./../../../assets/icons/Timer.png')
 interface RecommendationBoxProps {
-    icon: React.FC<SvgProps>,
-    title: string,
-    content: string
+  icon: React.FC<SvgProps>;
+  title: string;
+  content: string;
 }
 
-export const RecommendationBox: React.FC<RecommendationBoxProps> = ({ icon: Icon, title, content }) => {
-    return (
-        <View style={style.container}>
-            <Icon />
-            <View style={style.textContainer}>
-                <CustomText style={style.title}>{title}</CustomText>
-                <CustomText style={style.textContent}>{content}</CustomText>
-            </View>
-
-        </View>
-    )
-}
+export const RecommendationBox: React.FC<RecommendationBoxProps> = ({
+  icon: Icon,
+  title,
+  content,
+}) => {
+  return (
+    <View style={style.container}>
+      <Icon />
+      <View style={style.textContainer}>
+        <CustomText style={style.title}>{title}</CustomText>
+        <CustomText style={style.textContent}>{content}</CustomText>
+      </View>
+    </View>
+  );
+};
