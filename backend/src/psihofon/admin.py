3ba90cc759fc33b5587d26eaffd69ada7cc9a0b1
n-lekic <n.lekic@vegait.rs>
2021-09-25 21:28:52 +0200

    Reorder questionnaire inlines

--- backend/src/psihofon/admin.py
+++ backend/src/psihofon/admin.py
@@ -42,8 +42,8 @@ class AnswerInline(TranslationStackedInline):
 @admin.register(Questionnaire)
 class QuestionnaireAdmin(TranslationAdmin):
     inlines = [
-        QuestionInline,
         AnswerInline,
+        QuestionInline,
     ]
 
 
