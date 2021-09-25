1255dfcc4ca9e4c01610303ccff8b432b28e5d60
n-lekic <n.lekic@vegait.rs>
2021-09-25 13:54:21 +0200

    Change default route and set email as username field

--- backend/src/psihofon/models/user.py
+++ backend/src/psihofon/models/user.py
@@ -1,5 +1,14 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+
+    email = models.EmailField(db_index=True, unique=True)
+
+    # used for login
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def get_username(self):
+        return self.email
