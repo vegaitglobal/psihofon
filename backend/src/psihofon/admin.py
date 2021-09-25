567dcee90ab16a8ce4c5c580707f5ceafd01db0e
Milos Roknic <roknic.milos.994@gmail.com>
2021-09-25 12:23:40 +0200

    Create UserAdmin

--- backend/src/psihofon/admin.py
+++ backend/src/psihofon/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from psihofon.models import Organization
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from psihofon.models import Organization, User
 
 
 @admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
     pass
