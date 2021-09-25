892ac2b6dd6b36508f7fbff9fc3ab26c00cfee18
Milos Roknic <roknic.milos.994@gmail.com>
2021-09-25 15:08:12 +0200

    Create SelfEmpowermentExercise model

--- backend/src/psihofon/admin.py
+++ backend/src/psihofon/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from psihofon.models import Organization, User
+from modeltranslation.admin import TranslationAdmin
+
+from psihofon.models import Organization, User, SelfEmpowermentExercise
 
 
 @admin.register(Organization)
-class OrganizationAdmin(admin.ModelAdmin):
+class OrganizationAdmin(TranslationAdmin):
     pass
 
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
+    pass
+
+
+@admin.register(SelfEmpowermentExercise)
+class SelfEmpowermentExerciseAdmin(TranslationAdmin):
     pass
