5e715d127824eea0847f833a75e1637232e4d72a
n-lekic <n.lekic@vegait.rs>
2021-09-25 15:11:09 +0200

    Rebase to development

--- backend/src/psihofon/models/__init__.py
+++ backend/src/psihofon/models/__init__.py
@@ -1,3 +1,4 @@
 from psihofon.models.user import User
 from psihofon.models.organization import Organization
 from psihofon.models.self_empowerment_exercise import SelfEmpowermentExercise
+from psihofon.models.crisis_exercise import CrisisExercise
