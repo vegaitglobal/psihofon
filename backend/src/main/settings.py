9fb71f78cc6e4c3febd859e70ac044bb20fd5e85
Milos Roknic <roknic.milos.994@gmail.com>
2021-09-26 01:07:32 +0200

    Set serbian language as default language and remove english language

--- backend/src/main/settings.py
+++ backend/src/main/settings.py
@@ -111,12 +111,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'sr-latn'
 
 LANGUAGES = (
-    ('en', _('English')),
-    ('sr-latn', _('Serbian')),
+    ('sr-latn', _('Srpski')),
 )
+
+MODELTRANSLATION_DEFAULT_LANGUAGE = 'sr-latn'
 
 TIME_ZONE = 'UTC'
 
