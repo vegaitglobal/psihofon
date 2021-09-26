ecffdc12766ec4f54d6afa4f859e2b9d35e262fc
n-lekic <n.lekic@vegait.rs>
2021-09-26 18:29:50 +0200

    Add wait_for_postgress in shell script

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+wait_for_postgres() {
+  # Adapted from https://docs.docker.com/compose/startup-order/
+
+  echo 'Waiting for PostgreSQL...'
+  until PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOSTNAME" -U "$DB_USERNAME" -d "$DB_NAME" -c '\q'; do
+    echo >&2 "Postgres is unavailable - sleeping"
+    sleep 1
+  done
+}
+
+wait_for_postgres
+
 set -ex
 
 echo "Starting Django project..."
