7c21ec1df423d1bb02938a67b19a682c350d0b17
Milan Petrovic <milanpdo@gmail.com>
2021-09-26 14:04:48 +0200

    Remove API url from constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,4 +2,3 @@ import {Dimensions} from 'react-native';
 
 export const HEIGHT = Dimensions.get('window').height;
 export const isTinyDevice = HEIGHT < 660;
-export const API_URL = 'https://psihofon.sitesstage.com/api/v1';
