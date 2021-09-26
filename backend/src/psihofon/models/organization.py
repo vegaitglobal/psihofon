992f53b861b6ae8b72103acf2b5f4cd2b6714895
Milos Roknic <roknic.milos.994@gmail.com>
2021-09-26 11:58:24 +0200

    Translate all translatable strings to Serbina (Latin) and remove 'auth' app from Django admin

--- backend/src/psihofon/models/organization.py
+++ backend/src/psihofon/models/organization.py
@@ -4,15 +4,15 @@ from django.utils.translation import gettext_lazy as _
 
 class Organization(models.Model):
     class Meta:
-        verbose_name = _('Organization')
-        verbose_name_plural = _('Organizations')
+        verbose_name = _('Organizacija')
+        verbose_name_plural = _('Organizacije')
 
     name = models.CharField(
-        verbose_name=_('name'),
+        verbose_name=_('naziv'),
         max_length=255,
     )
     city = models.CharField(
-        verbose_name=_('city'),
+        verbose_name=_('grad'),
         max_length=255,
     )
     website_url = models.URLField(
--- backend/src/psihofon/urls.py
+++ backend/src/psihofon/urls.py
@@ -6,8 +6,8 @@ from psihofon.views import (
 
 urlpatterns = [
     path('organizations/', OrganizationAPIView.as_view(), name='organizations'),
-    path('crisis-exercises/', CrisisExerciseAPIView.as_view(), name="crisis_exercises"),
+    path('crisis-exercises/', CrisisExerciseAPIView.as_view(), name='crisis_exercises'),
     path('self-empowerment-exercises/', SelfEmpowermentExerciseAPIView.as_view(), name='self_empowerment_exercises'),
-    path('mental-states/', MentalStateAPIView.as_view(), name="mental_state"),
-    path('questionnaire/', QuestionnaireAPIView.as_view(), name="mental_state"),
+    path('mental-states/', MentalStateAPIView.as_view(), name='mental_state'),
+    path('questionnaire/', QuestionnaireAPIView.as_view(), name='mental_state'),
 ]
